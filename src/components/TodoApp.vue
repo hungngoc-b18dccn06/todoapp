d03952c86b92dae08f75a0ebcf07a796b962f255
Lê Ngọc Hưng <96043346+sky-hungln@users.noreply.github.com>
2022-11-14 11:55:27 +0700

    add-todo-list

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,180 @@
+<template>
+    <div class="container" style="max-width: 600px">
+      <!-- Heading -->
+      <h2 class="text-center mt-5">SkyLab Todo-List</h2>
+  
+      <!-- Input -->
+      <div class="d-flex mt-5">
+        <input
+          type="text"
+          v-model="task"
+          placeholder="Enter task"
+          class="w-100 form-control"
+        />
+        <button class="btn btn-warning rounded-0" @click="submitTask">
+          SUBMIT
+        </button>
+      </div>
+  
+      <!-- Task table -->
+      <table class="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <th scope="col">Task</th>
+            <th scope="col" style="width: 120px">Status</th>
+            <th scope="col" class="text-center">#</th>
+            <th scope="col" class="text-center">#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(task, index) in tasks" :key="index">
+            <td>
+              <span :class="{ 'line-through': task.status === 'finished' }">
+                {{ task.name }}
+              </span>
+            </td>
+            <td>
+              <span
+                class="pointer noselect"
+                @click="changeStatus(index)"
+                :class="{
+                  'text-danger': task.status === 'to-do',
+                  'text-success': task.status === 'finished',
+                  'text-warning': task.status === 'in-progress',
+                }"
+              >
+                {{ capitalizeFirstChar(task.status) }}
+              </span>
+            </td>
+            <td class="text-center">
+              <div @click="deleteTask(index)">
+                <span class="fa fa-trash pointer"></span>
+              </div>
+            </td>
+            <td class="text-center">
+              <div @click="editTask(index)">
+                <p class="fa fa-pen pointer"></p>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HelloWorld",
+    props: {
+      msg: String,
+    },
+  
+    data() {
+      return {
+        task: "",
+        editedTask: null,
+        statuses: ["to-do", "in-progress", "finished"],
+        tasks: [
+          {
+            name: "Tohgashi reservation screen",
+            status: "to-do",
+          },
+          {
+            name: "Medipurse Calendar screen.",
+            status: "in-progress",
+          },
+          {
+            name: "Create YouTube video.",
+            status: "finished",
+          },
+          {
+            name: "Tohgashi reservation screen",
+            status: "to-do",
+          },
+          {
+            name: "Medipurse Calendar screen.",
+            status: "in-progress",
+          },
+          {
+            name: "Create YouTube video.",
+            status: "finished",
+          },
+          {
+            name: "Tohgashi reservation screen",
+            status: "to-do",
+          },
+          {
+            name: "Medipurse Calendar screen.",
+            status: "in-progress",
+          },
+          {
+            name: "Create YouTube video.",
+            status: "finished",
+          },
+          {
+            name: "Tohgashi reservation screen",
+            status: "to-do",
+          },
+          {
+            name: "Medipurse Calendar screen.",
+            status: "in-progress",
+          },
+          {
+            name: "Create YouTube video.",
+            status: "finished",
+          },
+        ],
+      };
+    },
+  
+    methods: {
+      capitalizeFirstChar(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1) ;
+      },
+      changeStatus(index) {
+        let newIndex = this.statuses.indexOf(this.tasks[index].status);
+        if (++newIndex > 2) newIndex = 0;
+        this.tasks[index].status = this.statuses[newIndex];
+      },
+      deleteTask(index) {
+        this.tasks.splice(index, 1);
+      },
+  
+      editTask(index) {
+        this.task = this.tasks[index].name;
+        this.editedTask = index;
+      },
+      submitTask() {
+        if (this.task.length === 0) return;
+        if (this.editedTask != null) {
+          this.tasks[this.editedTask].name = this.task;
+          this.editedTask = null;
+        } else {
+          this.tasks.push({
+            name: this.task,
+            status: "todo",
+          });
+        }
+        this.task = "";
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+  .noselect {
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently
+                                    supported by Chrome, Edge, Opera and Firefox */
+  }
+  .line-through {
+    text-decoration: line-through;
+  }
+  </style>
